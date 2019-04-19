89b7d134d23d5745cec0507c8284a6c980a2638a
Adphi <philippe.adrien.nousse@gmail.com>
2019-04-19 19:46:08 +0200

    fix well-know types param names

--- protoc-gen-swagger/genswagger/atlas_patch.go
+++ protoc-gen-swagger/genswagger/atlas_patch.go
@@ -138,8 +138,13 @@ The service-defined string used to identify a page of resources. A null value in
 					default:
 					}
 					// Replace resource_id with id
-				} else if strings.HasSuffix(param.Name, "id.resource_id") || strings.HasSuffix(param.Name, ".id") {
+				} else if strings.HasSuffix(param.Name, "id.resource_id") ||
+					strings.HasSuffix(param.Name, ".id") ||
+					strings.HasSuffix(param.Name, ".id.value") {
 					param.Name = "id"
+					fixedParams = append(fixedParams, param)
+				} else if strings.HasSuffix(param.Name, ".value"){
+					param.Name = strings.TrimSuffix(param.Name, ".value")
 					fixedParams = append(fixedParams, param)
 				} else {
 					// Gather ref in body.
